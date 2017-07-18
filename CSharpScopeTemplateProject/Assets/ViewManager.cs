3edba4b6d7fbceee7717a0123c39513d76dfc090
foldalma <dalma.foldesi@gmail.com>
2017-07-18 16:10:34 -0400

    Add custom view toggle for calibrating (in Display 3, via UI)

---
+++ CSharpScopeTemplateProject/Assets/ViewManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewManager : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnViewChanged(int currView) {
+		if (currView == 0) {
+			GameObject.Find ("Grid_UI_Camera").GetComponent<Camera> ().enabled = true;
+		} else {
+			GameObject.Find ("Grid_UI_Camera").GetComponent<Camera> ().enabled = false;
+		}
+	}
+
+
+}
